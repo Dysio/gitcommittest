9961c4bcc80166b80e6fa592a80928ba9b1c546d
Dysio <td.dyszewski@gmail.com>
2022-03-22 14:38:07 +0100

    func evoke with name as arg

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ def print_hi(name: str):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hi('name')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
